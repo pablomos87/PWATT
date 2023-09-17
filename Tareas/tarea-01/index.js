620594481560f38464f2d0ecb740f2fdbc4bafec
pablomos <mosconpablo@gmail.com>
2023-09-17 11:40:43 -0300

    09/17 Commit

--- Tareas/tarea-01/index.js
+++ Tareas/tarea-01/index.js
@@ -6,9 +6,11 @@ const PORT = 8080;
 app.get("/", (req, res) => {
   console.log("Peticion bienvenida");
     const hourDate = new Date().getHours();
+
+    
     if (hourDate >= 6 && hourDate < 12) {
       res.send("<h1 style=color:green> Buenos días! </h1>");
-    } else if (hourDate >= 12 && hourDate < 19) {
+    } else if (hourDate >= 12 && hourDate < 18) {
       res.send("<h1 style=color:blue> Buenas tardes! </h1>");
     } else {
       res.send("<h1 style=color:red> Buenas noches! </h1>");
@@ -16,5 +18,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log("Servidor escuchando en el puerto ${PORT}");
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
